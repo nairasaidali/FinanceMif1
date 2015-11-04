217c76d1d64e7119ba711cf167acbc1f5d3f1d0a
Stann92 <victor.akyol@gmail.com>
2015-11-04 10:26:59 +0100

    J'ai rajouté un commentaire

--- src/main/java/com/sdesilv4/model/Indice.java
+++ src/main/java/com/sdesilv4/model/Indice.java
@@ -7,7 +7,7 @@ public class Indice extends Actif
  public Indice(String _nom, String _codeISIN, double _prix, long _volume, Date _date) {
   super(_nom, _codeISIN, _prix, _volume, _date);
  }
-
+//gjgjkbkjbkjb
 
 }
 
