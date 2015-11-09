2919dc26329afc7e979c5bc91a12f016876c0f33
NicolasGH <nicogheung@gmail.com>
2015-11-09 19:49:50 +0100

    Rajout petite exception

--- src/main/java/com/sdesilv4/Controller/ControllerGestionIndice.java
+++ src/main/java/com/sdesilv4/Controller/ControllerGestionIndice.java
@@ -159,6 +159,7 @@ public class ControllerGestionIndice {
              HttpRequest = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + URLEncoder.encode(symbolIndex, "UTF-8")  + "%22)&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=";
         }
         catch (UnsupportedEncodingException e) {
+            throw new AssertionError("UTF-8 is unknown");
         }
             JSONObject obj = new JSONObject(httsGetMethodJson(HttpRequest));
             JSONObject query = obj.getJSONObject("query");
--- src/main/java/com/sdesilv4/Controller/MongoDBClient.java
+++ src/main/java/com/sdesilv4/Controller/MongoDBClient.java
@@ -21,159 +21,128 @@ import javax.print.Doc;
 /**
  * Created by Bizeray on 06/11/2015.
  */
-public class MongoDBClient
-{
+public class MongoDBClient {
     private String uriString;
     private MongoClient connectionClient;
     private MongoClientURI uri;
     private MongoDatabase db;
 
-    public MongoDBClient(String _uriString)
-    {
+    public MongoDBClient(String _uriString) {
         this.uriString = _uriString;
         this.uri = new MongoClientURI(this.uriString);
         this.connectionClient = new MongoClient((uri));
         this.db = connectionClient.getDatabase(uri.getDatabase());
     }
 
-    public void CloseClientConnection()
-    {
+    public void CloseClientConnection() {
         this.connectionClient.close();
     }
 
-    public void AddAction(Action act)
-    {
-        try
-        {
+    public void AddAction(Action act) {
+
             MongoCollection coll = db.getCollection("action");
 
             List<Document> indexWeight = new ArrayList<Document>();
-            for (Map.Entry<String, Object> entry : act.getdictionary().entrySet())
-            {
+            for (Map.Entry<String, Object> entry : act.getdictionary().entrySet()) {
                 indexWeight.add(new Document(entry.getKey(), new Document("symbol", entry.getKey()).append("weight", entry.getValue())));
             }
             Document doc = new Document().append("name", act.getNom())
                     .append("codeISIN", act.getCodeISIN())
                     .append("prix", act.getPrix())
-                    .append("date",act.getDate())
+                    .append("date", act.getDate())
                     .append("volatility", act.getVolatility())
-                    .append("volume",act.getVolume())
+                    .append("volume", act.getVolume())
                     .append("cap_boursiere", act.getCapBoursiere())
                     .append("PER", act.getPER())
-                    .append("indexWeight", (indexWeight))
-                    ;
-
-
+                    .append("indexWeight", (indexWeight));
 
 
             coll.insertOne(doc);
-        }
-        catch (Exception e)
-        {  System.out.println(e.getMessage());}
+
 
     }
 
-    public void AddActionCollection(List<Action> collection)
-    {
+    public void AddActionCollection(List<Action> collection) {
         List<Document> listDocuments = new ArrayList<Document>();
-            MongoCollection coll = db.getCollection("action");
+        MongoCollection coll = db.getCollection("action");
 
-        for (Action a : collection)
-        {
+        for (Action a : collection) {
             List<Document> indexWeight = new ArrayList<Document>();
-            for (Map.Entry<String, Object> entry : a.getdictionary().entrySet())
-            {
+            for (Map.Entry<String, Object> entry : a.getdictionary().entrySet()) {
                 indexWeight.add(new Document(entry.getKey(), new Document("symbol", entry.getKey()).append("weight", entry.getValue())));
             }
             Document doc = new Document().append("name", a.getNom())
                     .append("codeISIN", a.getCodeISIN())
                     .append("prix", a.getPrix())
-                    .append("date",a.getDate())
+                    .append("date", a.getDate())
                     .append("volatility", a.getVolatility())
-                    .append("volume",a.getVolume())
+                    .append("volume", a.getVolume())
                     .append("cap_boursiere", a.getCapBoursiere())
                     .append("PER", a.getPER())
-                    .append("indexWeight", (indexWeight))
-                    ;
+                    .append("indexWeight", (indexWeight));
             listDocuments.add(doc);
         }
-            coll.insertMany(listDocuments);
+        coll.insertMany(listDocuments);
 
     }
 
-    public void AddIndice(Indice ind)
-    {
-        try
-        {
-            MongoCollection coll = db.getCollection("indice");
+    public void AddIndice(Indice ind) {
+                MongoCollection coll = db.getCollection("indice");
             List<Document> listSymbolAction = new ArrayList<Document>();
-            for (Action a : ind.GetCollectionAction())
-            {
-                listSymbolAction.add(new Document("symbol", a.getSymbol()).append("name",a.getNom()).append("price" , a.getPrix()));
+            for (Action a : ind.GetCollectionAction()) {
+                listSymbolAction.add(new Document("symbol", a.getSymbol()).append("name", a.getNom()).append("price", a.getPrix()));
             }
 
             Document doc = new Document().append("name", ind.getNom())
                     .append("codeISIN", ind.getCodeISIN())
                     .append("prix", ind.getPrix())
-                    .append("date",ind.getDate())
+                    .append("date", ind.getDate())
                     .append("volume", ind.getVolume())
                     .append("components", listSymbolAction);
 
             coll.insertOne(doc);
             AddActionCollection(ind.GetCollectionAction());
-        }
-        catch (Exception e)
-        {  System.out.println(e.getMessage());}
+
 
 
     }
 
-    public Action FindAction(String CodeISIN)
-    {
-        try
-        {
-            MongoCollection coll = db.getCollection("action");
+    public Action FindAction(String CodeISIN) {
+        MongoCollection coll = db.getCollection("action");
 
-            FindIterable<Document> g = coll.find(new Document("codeISIN", CodeISIN));
-            String name = (String) g.first().get("name");
-            String codeISIN = (String) g.first().get("codeISIN");
-            Double prix = (Double) g.first().get("prix");
-            Date date = (Date) g.first().get("date");
-            Long volume = (Long) g.first().get("volume");
-            Double volatility = (Double) g.first().get("volatility");
-            Double cap_boursiere = (Double) g.first().get("cap_boursiere");
-            Double PER = (Double) g.first().get("PER");
+        FindIterable<Document> g = coll.find(new Document("codeISIN", CodeISIN));
+        String name = (String) g.first().get("name");
+        String codeISIN = (String) g.first().get("codeISIN");
+        Double prix = (Double) g.first().get("prix");
+        Date date = (Date) g.first().get("date");
+        Long volume = (Long) g.first().get("volume");
+        Double volatility = (Double) g.first().get("volatility");
+        Double cap_boursiere = (Double) g.first().get("cap_boursiere");
+        Double PER = (Double) g.first().get("PER");
 
-            Action act = new Action (name,codeISIN,prix,volume,date,"symbol",volatility,cap_boursiere,PER);    //symbol a compléter !!
-            return act;
-        }
-        catch(Exception e) {
-            return null;
-        }
+        Action act = new Action(name, codeISIN, prix, volume, date, "symbol", volatility, cap_boursiere, PER);    //symbol a compléter !!
+        return act;
+
     }
 
 
-    public Indice FindIndice(String CodeISIN)
-    {
-        try
-        {
-            MongoCollection coll = db.getCollection("indice");
+    public Indice FindIndice(String CodeISIN) {
+        MongoCollection coll = db.getCollection("indice");
 
-            FindIterable<Document> g = coll.find(new Document("codeISIN", CodeISIN));
-            String name = (String) g.first().get("name");
-            String codeISIN = (String) g.first().get("codeISIN");
-            Double prix = (Double) g.first().get("prix");
-            Date date = (Date) g.first().get("date");
-            Long volume = (Long) g.first().get("volume");
-            Double volatility = (Double) g.first().get("volatility");
+        FindIterable<Document> g = coll.find(new Document("codeISIN", CodeISIN));
+        String name = (String) g.first().get("name");
+        String codeISIN = (String) g.first().get("codeISIN");
+        Double prix = (Double) g.first().get("prix");
+        Date date = (Date) g.first().get("date");
+        Long volume = (Long) g.first().get("volume");
+        Double volatility = (Double) g.first().get("volatility");
 
-            Indice ind = new Indice(name,codeISIN,prix,volume,date , "symbol !! "); //symbol a compléter !!
-            return ind;
-        }
-        catch(Exception e) {
-            return null;
-        }
+        Indice ind = new Indice(name, codeISIN, prix, volume, date, "symbol !! "); //symbol a compléter !!
+        return ind;
+
     }
+
+
 
 
 
@@ -187,7 +156,7 @@ public class MongoDBClient
 
 
     public String UpdateAction(Action act) {
-        try {
+
             MongoCollection coll = db.getCollection("action");
 
             Document doc = new Document().append("name", act.getNom())
@@ -202,15 +171,14 @@ public class MongoDBClient
             coll.updateOne(new Document("codeISIN", act.getCodeISIN()), doc);
 
 
-        } catch (Exception e) {
-            return e.getMessage();
-        }
+
 
         return "action bien modifier";
     }
 
     public String UpdateIndice(Indice ind) {
-        try {
+
+
             MongoCollection coll = db.getCollection("indice");
 
             Document doc = new Document().append("name", ind.getNom())
@@ -221,9 +189,7 @@ public class MongoDBClient
 
             coll.updateOne(new Document("codeISIN", ind.getCodeISIN()), doc);
 
-        } catch (Exception e) {
-            return e.getMessage();
-        }
+
 
         return "indice bien modifier";
     }
