29ddfb0dfc682c7ad3da0d43f46a72e3212dbe74
Bilale <bilale.harouna_abassi@devinci.fr>
2015-11-29 15:49:15 +0100

    Ajout de la test sur FindIndice

--- src/test/java/com/sdesilv4/Controller/MongoDBClientTest.java
+++ src/test/java/com/sdesilv4/Controller/MongoDBClientTest.java
@@ -1,6 +1,7 @@
 package com.sdesilv4.Controller;
 
 import com.sdesilv4.model.Action;
+import com.sdesilv4.model.Indice;
 import junit.framework.TestCase;
 
 import java.util.Date;
@@ -10,12 +11,12 @@ import java.util.Date;
  */
 public class MongoDBClientTest extends TestCase {
 
-    public void testFindAction() throws Exception {
+   public void testFindAction() throws Exception {
 
         MongoDBClient tst = new MongoDBClient("mongodb://groupeMif1:groupeMif1@ds051524.mongolab.com:51524/actif");
         Action act = tst.FindAction("CREDIT AGRICOLE");
-        //assertEquals(6361456, act.getVolume());   //Retourne bien une erreur et indique la bonne valeur du volume
-        assertEquals(6361466, act.getVolume());
+        assertEquals(6361456, act.getVolume());   //Retourne bien une erreur et indique la bonne valeur du volume
+        //assertEquals(6361466, act.getVolume());
     }
 
     public void testAddAction() throws Exception {
@@ -25,5 +26,13 @@ public class MongoDBClientTest extends TestCase {
 
         Action actNike = tst.FindAction("NIKE");    //Test si l'Action a été ajoutée
         assertEquals(6525990, actNike.getVolume()); //Vérifie si on a bien la meme valeur pour le volume
+    }
+
+       public void testFindIndice() throws Exception {
+        MongoDBClient tst = new MongoDBClient("mongodb://groupeMif1:groupeMif1@ds051524.mongolab.com:51524/actif");
+        Indice inde=tst.FindIndice("Not available in Yahoo API");
+
+        assertEquals("Not available in Yahoo API",inde.getCodeISIN());
+           assertEquals(4930.14,inde.getPrix());
     }
 }
